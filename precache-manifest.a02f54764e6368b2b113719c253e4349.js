ae2cf590a2bc77897232ef9abe08c67d44ca3373
CLAUDIA LILIANA ESTUPINAN FORERO <claudiainbytes@MacBook-Air-de-CLAUDIA-2.local>
2019-03-28 10:51:25 -0500

    Updates

---
+++ precache-manifest.a02f54764e6368b2b113719c253e4349.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "866d1bcf4fc8a9923332",
+    "url": "/reactjsweatherforecast/static/css/main.f9348ecf.chunk.css"
+  },
+  {
+    "revision": "866d1bcf4fc8a9923332",
+    "url": "/reactjsweatherforecast/static/js/main.872104f2.chunk.js"
+  },
+  {
+    "revision": "54f0013dd17a24886154",
+    "url": "/reactjsweatherforecast/static/js/runtime~main.83aef3be.js"
+  },
+  {
+    "revision": "6eb29c14bf010b81afff",
+    "url": "/reactjsweatherforecast/static/js/2.21b78771.chunk.js"
+  },
+  {
+    "revision": "64c9d18f80b05193647ce0e70c8cd18b",
+    "url": "/reactjsweatherforecast/index.html"
+  }
+];
